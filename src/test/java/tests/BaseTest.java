82076e4b4243c942ec5729f3945cbc3ccdc221c6
Usevalad <avm012@gmail.com>
2025-02-16 11:50:16 +0000

    16feb2025

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,7 +3,6 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.github.javafaker.Faker;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
--- src/test/java/tests/api/specs/Specs.java
+++ src/test/java/tests/api/specs/Specs.java
@@ -10,12 +10,14 @@ import static io.restassured.RestAssured.with;
 
 public class Specs {
 
+    public static String token = System.getProperty("token", "8712b8dd5d9089dac78e3e50b649233346b7f122015027677ef032f5b55fc9e8");
+
     public static final RequestSpecification REQ_SPEC = with()
             .baseUri("https://api.qase.io")
             .basePath("/v1")
             .log().uri()
             .contentType(ContentType.JSON)
-            .header("Token", "8712b8dd5d9089dac78e3e50b649233346b7f122015027677ef032f5b55fc9e8");
+            .header("Token", token);  //.header("Token", "8712b8dd5d9089dac78e3e50b649233346b7f122015027677ef032f5b55fc9e8");
 
     public static final ResponseSpecification RES_SPEC = new ResponseSpecBuilder()
             .log(LogDetail.ALL)

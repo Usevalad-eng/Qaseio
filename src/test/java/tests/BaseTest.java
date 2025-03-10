8cd0ecff654b9eaac423e38d0dae8739e6902df1
Usevalad <avm012@gmail.com>
2025-03-10 11:46:59 +0000

    10march2025

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -39,7 +39,7 @@ public class BaseTest {
     static void screenResolution() {
         Configuration.baseUrl = ConfigReader.projectConfig.baseUrl();
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        Configuration.headless = true;
         //Configuration.browser="edge";
     }
 
